a2e722cb2ddbad86de9bf932345925f1fd85e88e
Alba Veloso <albaveloso92@gmail.com>
2025-01-16 16:49:42 +0100

    inicio app

---
+++ registro.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="">Nombre</label>
+        <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+        <label for="">Email</label>
+        <input type="text" name="email" id="email" placeholder="Email">
+        <label for="">Contraseña</label>
+        <input type="password" name="password" id="password" placeholder="Contraseña">
+        <input type="submit" value="Registrar">
+    </form>
+    
+</body>
+</html>
